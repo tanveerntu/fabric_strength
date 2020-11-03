6d0ee7b4d83b9ed2e0420125e736f5868a4ebab2
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2020-11-03 10:55:02 +0500

    Update fabric_strength.py

--- fabric_strength.py
+++ fabric_strength.py
@@ -15,9 +15,9 @@ wp_material = st.selectbox('Warp Material', ('Cotton', 'P/C'))
 wt_material = st.selectbox('Weft Material', ('Cotton', 'P/C'))
 a = st.slider('Warp Yarn Strength (cN)', 270, 540, 400)  # 👈 this is a widget
 b = st.slider('Weft Yarn Strength (cN)', 270, 540, 400)
-c = st.slider('Ends per cm', 40, 80, 60)
-d = st.slider('Picks per cm', 40, 80, 60)
-e = st.slider('Fabric Float Length', 1, 3, 2)
+c = st.slider('Ends per inch', 40, 80, 60)
+d = st.slider('Picks per inch', 40, 80, 60)
+e = st.slider('Float Length: e.g. select 1 for 1/1 plain weave; 2 for 2/1 twill; 3 for 3/1 twill', 1, 3, 2)
 
 #a = wpYs; b = wtYs; c = E; d = P; e=FL
 # m1wp[warp fabric strength] and m1wt[weft fabric strength], m1wt equation is for pc warp, pc weft
@@ -75,27 +75,29 @@ wty_material = st.selectbox('Weft Yarn Material', ('Cotton', 'P/C'))
 
 #To create sliders on sidebar
 
-X = st.slider('Warp Yarn linear density (tex)', 15, 30, 20)  # 👈 this is a widget
-Y = st.slider('Weft Yarn linear density (tex)', 15, 30, 20)
+X = st.slider('Warp Yarn linear density (Ne)', 20, 40, 30)  # 👈 this is a widget
+Y = st.slider('Weft Yarn linear density (Ne)', 20, 40, 30)
+X1 = 590.5/X
+Y1 = 590.5/Y
 
 FTSwp = st.slider('Fabric Warp Strength (N)', 130, 850, 400)
 FTSwt = st.slider('Fabric Weft Strength (N)', 130, 850, 400)
 
-E = st.slider('Ends/ cm', 40, 80, 60)
-P = st.slider('Picks/ cm', 40, 80, 60)
-FL = st.slider('Float Length', 1, 3, 2)
+E = st.slider('Ends/ inch', 40, 80, 60)
+P = st.slider('Picks/ inch', 40, 80, 60)
+FL = st.slider('Float Length: e.g. select 1 for 1/1 plain weave; 2 for 2/1 twill; 3 for 3/1 twill', 1, 3, 2)
 
-YSwp1 = int((FTSwp)+(15.07)-(2.29 * Y)+(0.72 * E)-(1.22 * P)+(14.9 * FL))/((0.02 * E)-(0.37)) 
-YSwt1 = int((FTSwt)+(48.85)-(1.84 * X)-(0.85 * E)-(0.14 * P)+(14.01 * FL))/((0.02 * P)-(0.23)) 
+YSwp1 = int((FTSwp)+(15.07)-(2.29 * Y1)+(0.72 * E)-(1.22 * P)+(14.9 * FL))/((0.02 * E)-(0.37)) 
+YSwt1 = int((FTSwt)+(48.85)-(1.84 * X1)-(0.85 * E)-(0.14 * P)+(14.01 * FL))/((0.02 * P)-(0.23)) 
 
-YSwp2 = int((FTSwp)+(24.75)-(2.65 * Y)+(0.76 * E)-(0.89 * P)+(16.5 * FL))/((0.023 * E)-(0.37))
-YSwt2 = int((FTSwt)+(173.16)-(1.41 * X)-(0.96 * E)-(1.79 * P)+(10.35 * FL))/((0.014 * P)+(0.08))
+YSwp2 = int((FTSwp)+(24.75)-(2.65 * Y1)+(0.76 * E)-(0.89 * P)+(16.5 * FL))/((0.023 * E)-(0.37))
+YSwt2 = int((FTSwt)+(173.16)-(1.41 * X1)-(0.96 * E)-(1.79 * P)+(10.35 * FL))/((0.014 * P)+(0.08))
 
-YSwp3 = int((FTSwp)-(50.37)-(1.21 * Y)+(1.85 * E)-(0.88 * P)+(13.8 * FL))/((0.025 * E)-(0.5)) 
-YSwt3 = int((FTSwt)-(36.95)-(0.54 * X)-(0.72 * E)+(1.65 * P)+(13.51 * FL))/((0.023 * P)-(0.38)) 
+YSwp3 = int((FTSwp)-(50.37)-(1.21 * Y1)+(1.85 * E)-(0.88 * P)+(13.8 * FL))/((0.025 * E)-(0.5)) 
+YSwt3 = int((FTSwt)-(36.95)-(0.54 * X1)-(0.72 * E)+(1.65 * P)+(13.51 * FL))/((0.023 * P)-(0.38)) 
 
-YSwp4 = int((FTSwp)-(53.56)-(1.12 * Y)-(0.97 * E)-(0.59 * P)+(12.8 * FL))/((0.015 * E)-(0.38)) 
-YSwt4 = int((FTSwt)+(38.36)-(0.94 * X)-(0.42 * E)-(1.04 * P)+(12.4 * FL))/((0.018 * P)-(0.16)) 
+YSwp4 = int((FTSwp)-(53.56)-(1.12 * Y1)-(0.97 * E)-(0.59 * P)+(12.8 * FL))/((0.015 * E)-(0.38)) 
+YSwt4 = int((FTSwt)+(38.36)-(0.94 * X1)-(0.42 * E)-(1.04 * P)+(12.4 * FL))/((0.018 * P)-(0.16)) 
 
 #To show results on the main page, under the heading 
 left_column, right_column = st.beta_columns(2)
