bb02cedff0500241dd3441b1112518608a70bed4
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2021-10-20 11:44:33 +0500

    Update fabric_strength.py

--- fabric_strength.py
+++ fabric_strength.py
@@ -9,6 +9,15 @@ st.set_page_config(page_title="Pakistan Textile Exports",
 
 )
 
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 #Title on main page
 
