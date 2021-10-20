748893ed74da937a6dd7aef37158d37e3f8b2b42
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2021-10-20 10:21:45 +0500

    Update fabric_strength.py

--- fabric_strength.py
+++ fabric_strength.py
@@ -11,113 +11,108 @@ import pandas as pd
 st.title("Prediction of Fabric Tensile Strength")
 
 #To create sliders on sidebar
-wp_material = st.selectbox('Warp Material', ('Cotton', 'P/C'))
-wt_material = st.selectbox('Weft Material', ('Cotton', 'P/C'))
-a = st.slider('Warp Yarn Strength (cN)', 270, 540, 400)  # 👈 this is a widget
-b = st.slider('Weft Yarn Strength (cN)', 270, 540, 400)
-c = st.slider('Ends per inch', 40, 80, 60)
-d = st.slider('Picks per inch', 40, 80, 60)
-e = st.slider('Float Length: Select 1 for 1/1 plain weave; 2 for 2/1 twill; 3 for 3/1 twill', 1, 3, 2)
+#To show results on the main page, under the heading 
+left_column, right_column = st.columns(2)
+
+wp_material = left_column.selectbox('Select Warp Material', ('Cotton', 'P/C'))
+wt_material = right_column.selectbox('Select Weft Material', ('Cotton', 'P/C'))
+a = st.slider('Warp Yarn Strength (cN)', 310, 540, 400)  # 👈 this is a widget
+b = st.slider('Weft Yarn Strength (cN)', 310, 540, 400)
+c = st.slider('Ends per in', 40, 80, 60)
+d = st.slider('Picks per in', 40, 80, 60)
+e = st.slider('Fabric Float Length', 1, 3, 2)
 
 #a = wpYs; b = wtYs; c = E; d = P; e=FL
 # m1wp[warp fabric strength] and m1wt[weft fabric strength], m1wt equation is for pc warp, pc weft
 m1wp = int((-5) -(0.37 * a)+(0.09 * b)-(0.72 * c)+(1.22 * d)-(14.89 * e)+(0.02 * c * a))
 m1wt = int((-41) +(0.07 * a)-(0.23 * b)+(0.85 * c)+(0.15 * d)-(14.01 * e)+(0.02* d * b))
 
-#FTSwp = - 5 - 0.37YSwp + 0.09YSwt - 0.72E + 1.22P - 14.89FL + 0.02E *YSwp
-#FTSwt = - 41 + 0.07YSwp - 0.23YSwt + 0.85E + 0.15P - 14.01FL + 0.02P *YSwt
 
 #warp PC, weft cotton
 m2wp = int((- 14.63) -(0.37 * a)+(0.14 * b)-(0.76 * c)+(0.89 * d)-(16.5 * e)+(0.023 * c * a))
 m2wt = int((- 167.43) +(0.05 * a)+(0.08 * b)+(0.96 * c)+(1.8 * d)-(10.35 * e)+(0.014* d * b))
 
-#FTSwp= - 14.63 - 0.37YSwp + 0.14 YSwt - 0.76E + 0.89P – 16.5FL + 0.023E*YSwp
-#FTSwt = - 167.43 + 0.05YSwp + 0.08YSwt + 0.96E + 1.8P - 10.35FL + 0.014P *YSwt 
 #warp cotton, weft cotton
 m3wp = int((55.01) -(0.5 * a)+(0.06 * b)-(1.85 * c)+(0.88 * d)-(13.8 * e)+(0.025 * c * a))
 m3wt = int((37.1) +(0.03 * a)-(0.38 * b)+(0.72 * c)-(1.64 * d)-(13.5 * e)+(0.023* d * b))
-
-#FTSwp = 55.01 - 0.5YSwp+ 0.06YSwt – 1.85E + 0.88P – 13.8FL + 0.025E*YSwp 
-#FTSwt = 37.1+ 0.03YSwp - 0.38YSwt + 0.72E - 1.64P - 13.5FL + 0.023P*YSwt 
 
 #warp cotton, weft pc
 m4wp = int((58.77) -(0.38 * a)+(0.04 * b)+(0.97 * c)+(0.59 * d)-(12.8 * e)+(0.015 * c * a))
 m4wt = int((- 33.07) +(0.05 * a)-(0.16 * b)+(0.42 * c)+(1.04 * d)-(12.4 * e)+(0.018* d * b))
 
-#FTSwp = 58.77 - 0.38YSwp + 0.04YSwt + 0.97E + 0.59P - 12.8FL + 0.015E*YSwp 
-#FTSwt = - 33.07 + 0.05YSwp - 0.16YSwt + 0.42E + 1.04P - 12.4FL + 0.018P*YSwt 
+left_column1, right_column1 = st.columns(2)
 
-
-#To show results on the main page, under the heading 
-left_column, right_column = st.columns(2)
-left_column.subheader("Fabric Tensile Strength (Warp), N")
-right_column.subheader("Fabric Tensile Strength (Weft), N")
 
 if wp_material =='P/C' and wt_material =='P/C': 
-    left_column.write(m1wp)
-    right_column.write(m1wt)
+    left_column1.metric(label = "Predicted Fabric Tensile Strength (Warp), N", value = m1wp)
+    right_column1.metric(label = "Predicted Fabric Tensile Strength (Weft), N", value = m1wt)
 elif  wp_material =='P/C' and wt_material =='Cotton':
-    left_column.write(m2wp)
-    right_column.write(m2wt)
+    left_column1.metric(label = "Predicted Fabric Tensile Strength (Warp), N", value = m2wp)
+    right_column1.metric(label = "Predicted Fabric Tensile Strength (Weft), N", value = m2wt)
 elif  wp_material =='Cotton' and wt_material =='Cotton':
-    left_column.write(m3wp)
-    right_column.write(m3wt)
+    left_column1.metric(label = "Predicted Fabric Tensile Strength (Warp), N", value = m3wp)
+    right_column1.metric(label = "Predicted Fabric Tensile Strength (Weft), N", value = m3wt)
 elif  wp_material =='Cotton' and wt_material =='P/C':
-    left_column.write(m4wp)
-    right_column.write(m4wt)
-    
+    left_column1.metric(label = "Predicted Fabric Tensile Strength (Warp), N", value = m4wp)
+    right_column1.metric(label = "Predicted Fabric Tensile Strength (Weft), N", value = m4wt)
+
+
+
 #Part 2: for prediction of yarn strength
-st.title("Prediction of Yarn Strength")
+st.title("Prediction of Yarn Tensile Strength")
 st.write("for producing fabric of specific strength")
-wpy_material = st.selectbox('Warp Yarn Material', ('Cotton', 'P/C'))
-wty_material = st.selectbox('Weft Yarn Material', ('Cotton', 'P/C'))
+
+left_column3, right_column3 = st.columns(2)
+
+wpy_material = left_column3.selectbox('Select Warp Yarn Material', ('Cotton', 'P/C'))
+wty_material = right_column3.selectbox('Select Weft Yarn Material', ('Cotton', 'P/C'))
 
 
 #To create sliders on sidebar
 
-X = st.slider('Warp Yarn Count (Ne)', 20, 40, 30)  # 👈 this is a widget
-Y = st.slider('Weft Yarn Count (Ne)', 20, 40, 30)
+X = st.slider('Warp Yarn linear density (Ne)', 20, 40, 30)  # 👈 this is a widget
+Y = st.slider('Weft Yarn linear density (Ne)', 20, 40, 30)
 X1 = 590.5/X
 Y1 = 590.5/Y
 
 FTSwp = st.slider('Fabric Warp Strength (N)', 130, 850, 400)
 FTSwt = st.slider('Fabric Weft Strength (N)', 130, 850, 400)
 
-E = st.slider('Ends/ inch', 40, 80, 60)
-P = st.slider('Picks/ inch', 40, 80, 60)
-FL = st.slider('Float Length: e.g. select 1 for 1/1 plain weave; 2 for 2/1 twill; 3 for 3/1 twill', 1, 3, 2)
+E = st.slider('Ends/ in', 40, 80, 60)
+P = st.slider('Picks/ in', 40, 80, 60)
+FL = st.slider('Float Length', 1, 3, 2)
 
-YSwp1 = int((FTSwp)+(15.07)-(2.29 * Y1)+(0.72 * E)-(1.22 * P)+(14.9 * FL))/((0.02 * E)-(0.37)) 
-YSwt1 = int((FTSwt)+(48.85)-(1.84 * X1)-(0.85 * E)-(0.14 * P)+(14.01 * FL))/((0.02 * P)-(0.23)) 
+YSwp1 = int ((FTSwp)+(15.07)-(2.29 * Y1)+(0.72 * E)-(1.22 * P)+(14.9 * FL))/((0.02 * E)-(0.37)) 
+YSwt1 = int ((FTSwt)+(48.85)-(1.84 * X1)-(0.85 * E)-(0.14 * P)+(14.01 * FL))/((0.02 * P)-(0.23)) 
 
-YSwp2 = int((FTSwp)+(24.75)-(2.65 * Y1)+(0.76 * E)-(0.89 * P)+(16.5 * FL))/((0.023 * E)-(0.37))
-YSwt2 = int((FTSwt)+(173.16)-(1.41 * X1)-(0.96 * E)-(1.79 * P)+(10.35 * FL))/((0.014 * P)+(0.08))
+YSwp2 = int ((FTSwp)+(24.75)-(2.65 * Y1)+(0.76 * E)-(0.89 * P)+(16.5 * FL))/((0.023 * E)-(0.37))
+YSwt2 = int ((FTSwt)+(173.16)-(1.41 * X1)-(0.96 * E)-(1.79 * P)+(10.35 * FL))/((0.014 * P)+(0.08))
 
-YSwp3 = int((FTSwp)-(50.37)-(1.21 * Y1)+(1.85 * E)-(0.88 * P)+(13.8 * FL))/((0.025 * E)-(0.5)) 
-YSwt3 = int((FTSwt)-(36.95)-(0.54 * X1)-(0.72 * E)+(1.65 * P)+(13.51 * FL))/((0.023 * P)-(0.38)) 
+YSwp3 = int ((FTSwp)-(50.37)-(1.21 * Y1)+(1.85 * E)-(0.88 * P)+(13.8 * FL))/((0.025 * E)-(0.5)) 
+YSwt3 = int ((FTSwt)-(36.95)-(0.54 * X1)-(0.72 * E)+(1.65 * P)+(13.51 * FL))/((0.023 * P)-(0.38)) 
 
-YSwp4 = int((FTSwp)-(53.56)-(1.12 * Y1)-(0.97 * E)-(0.59 * P)+(12.8 * FL))/((0.015 * E)-(0.38)) 
-YSwt4 = int((FTSwt)+(38.36)-(0.94 * X1)-(0.42 * E)-(1.04 * P)+(12.4 * FL))/((0.018 * P)-(0.16)) 
+YSwp4 = int ((FTSwp)-(53.56)-(1.12 * Y1)-(0.97 * E)-(0.59 * P)+(12.8 * FL))/((0.015 * E)-(0.38)) 
+YSwt4 = int ((FTSwt)+(38.36)-(0.94 * X1)-(0.42 * E)-(1.04 * P)+(12.4 * FL))/((0.018 * P)-(0.16)) 
 
 #To show results on the main page, under the heading 
-left_column, right_column = st.columns(2)
-left_column.subheader("Warp Yarn Tensile Strength, cN")
-right_column.subheader("Weft Yarn Tensile Strength, cN")
+
+left_column2, right_column2 = st.columns(2)
+
 
 if wpy_material =='P/C' and wty_material =='P/C': 
-    left_column.write(YSwp1)
-    right_column.write(YSwt1)
+    left_column2.metric(label = "Predicted Warp Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwp1))
+    right_column2.metric(label = "Predicted Weft Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwt1))
 elif  wpy_material =='P/C' and wty_material =='Cotton':
-    left_column.write(YSwp2)
-    right_column.write(YSwt2)
+    left_column2.metric(label = "Predicted Warp Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwp2))
+    right_column2.metric(label = "Predicted Weft Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwt2))
 elif  wpy_material =='Cotton' and wty_material =='Cotton':
-    left_column.write(YSwp3)
-    right_column.write(YSwt3)
+    left_column2.metric(label = "Predicted Warp Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwp3))
+    right_column2.metric(label = "Predicted Weft Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwt3))
 elif  wpy_material =='Cotton' and wty_material =='P/C':
-    left_column.write(YSwp4)
-    right_column.write(YSwt4)
-    
-st.info("Courtesy: Dr. Zulfikar Ali, National Textile University")
+    left_column2.metric(label = "Predicted Warp Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwp4))
+    right_column2.metric(label = "Predicted Weft Yarn Tensile Strength, cN", value = "{:,.0f}".format(YSwt4))
+
+
 
 
 
